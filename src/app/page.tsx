7b9442675236fe6bb856f8c24eac4f6e00395a9c
asaldanam <asaldanam@gmail.com>
2024-03-17 10:38:13 +0100

    chore: add libs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 export default function Home() {
     return (
         <div>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="picture">Picture</Label>
+                <Input id="picture" type="file" />
+            </div>
+
             <Button>Click me</Button>
         </div>
     );
