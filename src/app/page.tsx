06927d7b7e22367dd5f83e2ff2192219c662dec3
asaldanam <asaldanam@gmail.com>
2024-03-31 13:02:37 +0200

    remove notion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,5 @@
 import Link from 'next/link';
 
 export default function Page() {
-    return (
-        <main>
-            <ul className="px-4">
-                <li className="mt-4">
-                    <div>
-                        <div>Escribe tu historia</div>
-                        <Link className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="/editor">
-                            /editor
-                        </Link>
-                    </div>
-                </li>
-                <li className="mt-4">
-                    <div>
-                        <div>Publica tu historia para siempre en IPFS</div>
-                        <Link className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="/publish">
-                            /publish
-                        </Link>
-                    </div>
-                </li>
-                <li className="mt-4">
-                    <div>
-                        <div>Importa tu historia desde Notion</div>
-                        <Link className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="/import">
-                            /import
-                        </Link>
-                    </div>
-                </li>
-                <li className="mt-4">
-                    <div>
-                        <div>Para maquetar el lector de historias</div>
-                        <Link
-                            className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                            href="/playground"
-                        >
-                            /playground
-                        </Link>
-                    </div>
-                </li>
-            </ul>
-        </main>
-    );
+    return <main>Fabula.pub</main>;
 }
--- src/core/story/application/import/NotionStoryImporter.ts
+++
@@ -1,9 +0,0 @@
-import { Publication } from '../../domain/models/Publication/Publication';
-import { createStory } from '../../domain/models/Story/Story';
-import { FileStorage } from '../../domain/interfaces/FileStorage';
-
-export class NotionStoryImporter {
-    constructor(public readonly repository: FileStorage) {}
-
-    async import(data: any) {}
-}
